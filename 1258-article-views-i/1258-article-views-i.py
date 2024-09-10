9ba99131ec3aa4e82dc20393ddedea3b9250a584
Hartono <124220894+mas-tono@users.noreply.github.com>
2024-09-10 19:03:38 +0700

    Time: 491 ms (7.6%), Space: 69.3 MB (95%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.py
+++ 1258-article-views-i/1258-article-views-i.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+# create a check column where author_id = viewer_id
+# return only unique author_id from check column, rename the column, sort it by id
+
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    views['cek'] = views['author_id'] == views['viewer_id']
-    return views[views['cek'] == True][['author_id']].drop_duplicates().rename(columns={'author_id':'id'}).sort_values('id')
+    views['check'] = views['author_id'] == views['viewer_id']
+    return views[views['check'] == True][['author_id']].drop_duplicates().rename(columns={'author_id':'id'}).sort_values('id')
