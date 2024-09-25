99e5a3882ea617ddbbf8c5eb4c30ac02f3130bcd
Hartono <124220894+mas-tono@users.noreply.github.com>
2024-09-25 11:35:30 +0700

    Time: 339 ms (78.81%), Space: 69.5 MB (28.52%) - LeetHub

---
+++ 2885-count-occurrences-in-text/2885-count-occurrences-in-text.py
@@ -0,0 +1,13 @@
+#1. clean all leading and trailing whitespaces -> trim
+#2. the requirement stated 'standalone word' -> bull/bear at beginning and end doesn't count -> find, rfind
+#3. use r'\bbear\b' r'\bbull\b' to do the task
+
+import pandas as pd
+
+def count_occurrences(files: pd.DataFrame) -> pd.DataFrame:
+    files['trim'] = files['content'].str.strip()
+    files['content_2'] = files['trim'].apply(lambda x: x[x.find(' '):x.rfind(' ')])
+    files['bear'] = files['content_2'].apply(lambda x: 1 if re.search(r'\bbear\b', x) else 0)
+    files['bull'] = files['content_2'].apply(lambda x: 1 if re.search(r'\bbull\b', x) else 0)
+    c = pd.DataFrame({'word':['bull', 'bear'], 'count':[files['bull'].sum(), files['bear'].sum()]})
+    return c
